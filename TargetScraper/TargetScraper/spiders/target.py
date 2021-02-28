1cc2dea4e826e6cdbd756004b91cbe6e7045e1c3
ActiveConclusion <activeconclusioner@gmail.com>
2021-02-28 15:03:16 +0200

    scrape product page for building API request

--- TargetScraper/TargetScraper/spiders/target.py
+++ TargetScraper/TargetScraper/spiders/target.py
@@ -1,10 +1,46 @@
+import json
+import re
+
 import scrapy
+
+API_BASE_URL = "https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1"
 
 
 class TargetSpider(scrapy.Spider):
-    name = 'target'
-    allowed_domains = ['target.com']
-    start_urls = ['http://target.com/']
+    name = "target"
+    allowed_domains = ["target.com"]
+    start_urls = [
+        "https://www.target.com/p/reese-39-s-easter-peanut-butter-eggs-7-2oz-6ct/-/A-53957905"
+    ]
 
     def parse(self, response):
+        # scrape params for the API request
+        product_url = response.request.url
+        # get tcin after the last slash from the URL
+        tcin_raw = product_url.split("/")[-1]
+        # get the numeric part of tcin
+        tcin = tcin_raw.split("-")[1]
+
+        # extract api_key and pricing_store_id from page
+        api_key = re.search(r'"apiKey":"(.*?)"', response.text).group(1)
+        pricing_store_id = re.search(
+            r'"pricing_store_id":"(.*?)"', response.text
+        ).group(1)
+
+        # set api params
+        params = {
+            "key": api_key,
+            "tcin": tcin,
+            "store_id": "none",
+            "has_store_id": "false",
+            "pricing_store_id": pricing_store_id,
+            "scheduled_delivery_store_id": "none",
+            "has_scheduled_delivery_store_id": "false",
+            "has_financing_options": "false",
+        }
+        yield scrapy.FormRequest(
+            API_BASE_URL, method="GET", callback=self.parse_api, formdata=params
+        )
+
+    def parse_api(self, response):
         pass
